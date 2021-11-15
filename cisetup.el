25454802abf50950e2f8216edbb3d0019ead0d5e
mikepro <mikepro@users.noreply.github.com>
2021-11-15 21:16:36 +0000

    Initialize package

--- cisetup.el
+++ cisetup.el
@@ -1,5 +1,6 @@
 (require 'package)
 (add-to-list 'package-archives
              '("melpa-stable" . "http://stable.melpa.org/packages/") t)
-
+(package-initialize)
+(package-refresh-contents)
 (package-install 'buttercup)
